b940ac109e61ac68dd32bba31be40fee0266fba3
poroping <43107292+poroping@users.noreply.github.com>
2022-05-12 12:25:44 +0000

    Add port channel subinterface resource (#15)

---
+++ examples/resources/iosxe_interface_port_channel_subinterface/resource.tf
@@ -0,0 +1,8 @@
+resource "iosxe_interface_port_channel_subinterface" "example" {
+  name              = "10.666"
+  description       = "My Interface Description"
+  shutdown          = false
+  vrf_forwarding    = "VRF1"
+  ipv4_address      = "192.0.2.2"
+  ipv4_address_mask = "255.255.255.0"
+}
