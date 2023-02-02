ebe371899c4328d97b176a58302099904b9d454a
danischm <danischm@cisco.com>
2023-02-02 10:14:47 +0100

    Add another restconf example

--- examples/resources/iosxe_restconf/resource.tf
+++ examples/resources/iosxe_restconf/resource.tf
@@ -1,6 +1,22 @@
-resource "iosxe_restconf" "example" {
+resource "iosxe_restconf" "simple" {
   path = "Cisco-IOS-XE-native:native/banner/login"
   attributes = {
     banner = "My Banner"
   }
+}
+
+resource "iosxe_restconf" "nested_list" {
+  path = "Cisco-IOS-XE-native:native/ip"
+  attributes = {
+    source-route = "true"
+  }
+  lists = [{
+    name = "vrf"
+    key  = "name"
+    items = [{
+      attributes = {
+        name = "VRF1"
+      }
+    }]
+  }]
 }
